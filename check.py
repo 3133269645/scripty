9c1af083112316d9bef6718255784734431e41a7
xqn <3133269645@qq.com>
2025-09-02 02:06:20 +0800

    4

--- check.py
+++ check.py
@@ -26,9 +26,24 @@ PAG = {
 }
 
 HEADERS = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0",
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
     "app-id": "cnooc-kshmh",
+    "connection": "keep-alive",
+    "did": "0",
+    "host": "bid.cnooc.com.cn",
+    "is-english": "0",
+    "referer": "https://bid.cnooc.com.cn/home/",
+    "sec-ch-ua": '"Not;A=Brand";v="99", "Microsoft Edge";v="139", "Chromium";v="139"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0"
 }
+
 PARAMS = {
     "status": 3,
     "current": 1,
