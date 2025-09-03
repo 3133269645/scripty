fedc980873bd72652bc61524829f294e9da2e9a2
xqn <120449035+3133269645@users.noreply.github.com>
2025-09-03 19:11:26 +0800

    Update check.py

--- check.py
+++ check.py
@@ -36,7 +36,7 @@ HEADERS = {
     "sec-fetch-site": "same-origin",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0"
 }
-KEYWORD = ["中海油", "北斗", "人员定位", "工牌", "手持", "车载终端", "接收机", "监测", "短报文", "对讲机", "授时", "无人机", "机器人", "巡检"]
+KEYWORD = ["中海油", "北斗", "人员定位", "工牌", "手持", "车载终端", "接收机", "监测", "短报文", "对讲机", "授时", "无人机", "机器人", "巡检","补芯"]
 PUSH_TOKEN = os.getenv("PUSHPLUS_TOKEN", "f79e9d696bc745378ecb4ec8236abe83")
 # ======================================
 
